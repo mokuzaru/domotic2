3ea16366d4bdb671c9b90be23ab6ff9d8c1fe48c
mokuzaru <kenny.otoya@pucp.pe>
2019-03-21 23:22:55 -0500

    finish

--- main2.ino
+++ main2.ino
@@ -20,7 +20,8 @@
 
 //globales
 
-#define DHTPIN 3
+#define DHT1PIN 3
+#define DHT2PIN 4
 int mq = 30;
 int d0 = 22;
 int humedad = A0;
@@ -28,12 +29,15 @@ char l = 'k';
 int servo1 = 31;
 int servo2 = 32;
 int fan1 = 33;
+int fan2 = 34;
 int ldr = A1;
-int luzS = 4;
+int dimmer = 6;
 int luzL = 24;
 int luzC = 25;
-int luzC2 = 5;
+int dimmer2 = 5;
 int luzE = 27;
+int boton = 28;
+int bomb = 29;
 
 
 //Rango hum : 0~300.muy humedo # 300~700. intermedio #700~1023.seco
@@ -48,8 +52,9 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 //dht 11 shit
 
 #define DHTTYPE DHT11
-DHT dht (DHTPIN, DHTTYPE);
-float temp, hum;
+DHT dht (DHT1PIN, DHTTYPE);
+DHT dht2 (DHT2PIN, DHTTYPE);
+float temp, hum, temp2, hum2;
 
 //objetos
 
@@ -63,10 +68,18 @@ void setup() {
   SPI.begin();
   mfrc522.PCD_Init();
   pinMode(mq,INPUT);
-  pinMode(30, OUTPUT);
-  pinMode(31, OUTPUT);
-  pinMode(32, OUTPUT);
-  pinMode(33, OUTPUT);
+  pinMode(servo1, OUTPUT);
+  pinMode(servo2, OUTPUT);
+  pinMode(fan1, OUTPUT);
+  pinMode(fan2, OUTPUT);
+  pinMode(dimmer, OUTPUT);
+  pinMode(luzL, OUTPUT);
+  pinMode(luzC, OUTPUT);
+  pinMode(dimmer2, OUTPUT);
+  pinMode(ldr, INPUT);
+  pinMode(boton, INPUT);
+  pinMode(bomb, OUTPUT);
+  pinMode(humedad, INPUT);
 
   //put close
 
@@ -75,6 +88,10 @@ void setup() {
   //Initial mode
 
  digitalWrite(fan1, HIGH);
+ digitalWrite(fan2, HIGH);
+ digitalWrite(bomb, HIGH);
+ digitalWrite(dimmer, LOW);
+ digitalWrite(dimmer2, LOW);
   
 }
 
@@ -93,8 +110,17 @@ void shit(char a){
     break;
 
     case 'H':
-      //Start something
+      digitalWrite(bomb, LOW);
+      delay(1000);
+      digitalWrite(bomb, HIGH);
     break;
+
+    case 'q':
+      digitalWrite(fan2, LOW);
+      delay(1000);
+      digitalWrite(fan2, HIGH);
+    break;
+
   }
   return;
 }
@@ -103,21 +129,20 @@ void loop() {
   // Revisamos si hay nuevas tarjetas  presentes
   if ( mfrc522.PICC_IsNewCardPresent()){  
       //Seleccionamos una tarjeta
-            if ( mfrc522.PICC_ReadCardSerial()){
+            if ( mfrc522.PICC_ReadCardSerial() == "35A22E83"){
                   // Enviamos serialemente su UID
-                  Serial.print("Card UID:");
-                  for (byte i = 0; i < mfrc522.uid.size; i++) {
-                          Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-                          Serial.print(mfrc522.uid.uidByte[i], HEX);   
-                  } 
+//                  Serial.print("Card UID:");
+//                  for (byte i = 0; i < mfrc522.uid.size; i++) {
+//                          Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+//                          Serial.print(mfrc522.uid.uidByte[i], HEX);   
+                  
                   l = 'c';
                   shit(l);
-                  Serial.println();
+            }
+                  //Serial.println();
                   // Terminamos la lectura de la tarjeta  actual
                   mfrc522.PICC_HaltA();         
-             }      
-    }
-
+             }   
     if( analogRead(humedad) < 700 ){
       l = "H";
       shit(l);
@@ -135,12 +160,32 @@ void loop() {
       shit(l);
     }
 
+    hum2 = dht2.readHumidity(); 
+    temp2 = dht2.readTemperature();
+
+    if(isnan(hum2) || isnan (temp2)){
+      Serial.println("Fallo2!");
+    }else{
+      Serial.print(temp2);
+      Serial.print(hum2);
+      l = 'q';
+      shit(l);
+    }
+
     boolean humo = digitalRead(mq);
-    if ( mq){
+    if (humo){
       Serial.print ("good air");
     }else{
       l = 'q';
       shit(l);
     }
 
+    int luzfuera = analogRead(ldr);
+
+    if (luzfuera < 512){
+      analogWrite(dimmer, 1023);
+    }else {
+      digitalWrite(dimmer, LOW);
+    }
+    
 }
